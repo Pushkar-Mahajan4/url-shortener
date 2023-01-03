c2c37aa373161f99cb4d0fe666344a87b45055a3
Cassan0va27 <pushkarmahajan4@gmail.com>
2023-01-02 22:46:35 -0500

    Minor UI update

--- frontend/src/components/main-content/login-component/Login.js
+++ frontend/src/components/main-content/login-component/Login.js
@@ -1,6 +1,15 @@
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [event, setEvent] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    console.log(username);
+    console.log(password);
+  }
   return (
     // <main className="login-container">
     //   <div className="content-container">
@@ -23,34 +32,40 @@ const Login = () => {
     //   </div>
     // </main>
 
-      <section class="flex items-center justify-center min-h-full mt-12 w-full gradient-form">
-        <div class="flex items-center justify-center w-1/2 bg-white shadow-md mb-11 rounded ">
-          <div class="flex w-1/2 p-4 h-full text-white"> 
+      <section className="flex items-center justify-center min-h-full mt-12 w-full gradient-form">
+        <div className="flex items-center justify-center w-1/2 bg-white shadow-md mb-11 rounded ">
+          <div className="flex w-1/2 p-4 h-full text-white"> 
               <img src="https://cdn.dribbble.com/users/230290/screenshots/15128882/media/4175d17c66f179fea9b969bbf946820f.jpg?compress=1&resize=400x300"/>
           </div>
-          <div class="flex w-1/2 p-4 h-full text-white"> 
-              <form>
-              <div class="mb-4 ">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                  Username
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-              </div>
-              <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                  Password
-                </label>
-                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                <p class="text-red-500 text-xs italic">Please choose a password.</p>
-              </div>
-              <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                  Sign In
-                </button>
-                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                  Create account
-                </a>
-              </div>
+          <div className="flex w-1/2 p-4 h-full text-white"> 
+              <form onSubmit={() => handleSubmit()}>
+                  <div className="mb-4 ">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" name="username">
+                      Username
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                     id="username" type="text"
+                     placeholder="Username" name="username"
+                     onChange={(e) => setUsername(e.target.value)}/>
+                  </div>
+                  <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" name="password">
+                      Password
+                    </label>
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                     name="password" id="password" 
+                     type="password" placeholder="******************"
+                     onChange={(e) => setPassword(e.target.value)}/>
+                    <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                      Sign In
+                    </button>
+                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                      Create account
+                    </a>
+                  </div>
               </form>
           </div>  
         </div>
